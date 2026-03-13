979a8241e05315ccc27ccc4a7a067a0771b4f5ee
Josh Symonds <josh@joshsymonds.com>
2026-03-12 17:54:32 -0700

    use host keys

--- home-manager/ssh-agent/default.nix
+++ home-manager/ssh-agent/default.nix
@@ -64,10 +64,10 @@ in {
           IdentityAgent /run/user/1000/ssh-agent.socket
         ''}
 
-        # macOS specific: use 1Password SSH agent
+        # macOS specific: use system keychain for passphrase caching
         ${lib.optionalString pkgs.stdenv.isDarwin ''
           UseKeychain yes
-          IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+          IdentityFile ~/.ssh/github
         ''}
       '';
 

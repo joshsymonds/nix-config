3d92c4f836ce22964ad43fc3044bbdf90c016d74
Josh Symonds <josh@joshsymonds.com>
2026-03-18 19:39:08 -0500

    Fix brew

--- home-manager/zsh/default.nix
+++ home-manager/zsh/default.nix
@@ -45,6 +45,11 @@ in {
     };
 
     envExtra = ''
+      # Homebrew on Apple Silicon
+      if [[ -x /opt/homebrew/bin/brew ]]; then
+        eval "$(/opt/homebrew/bin/brew shellenv)"
+      fi
+
       # GPG agent needs this to find the terminal for pinentry
       export GPG_TTY=$(tty)
 

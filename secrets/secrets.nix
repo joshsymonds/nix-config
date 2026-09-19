15c8104a2ab6c770a0e70d3b93c08a4e302390fa
Josh Symonds <josh@joshsymonds.com>
2026-09-19 15:14:22 -0700

    valheim: register encrypted server password

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -61,6 +61,7 @@ in {
   "secrets/hosts/ultraviolet/savecraft-canary-fixture.age".publicKeys = keys.ultraviolet;
   "secrets/hosts/ultraviolet/savecraft-canary-access-client-id.age".publicKeys = keys.ultraviolet;
   "secrets/hosts/ultraviolet/savecraft-canary-access-client-secret.age".publicKeys = keys.ultraviolet;
+  "secrets/hosts/ultraviolet/valheim-password.age".publicKeys = keys.ultraviolet;
   "secrets/hosts/gnomon/mullvad-privatekey.age".publicKeys = keys.gnomon;
   "secrets/hosts/gnomon/mullvad-addresses.age".publicKeys = keys.gnomon;
   "secrets/hosts/vermissian/mullvad-privatekey.age".publicKeys = keys.vermissian;

87c2903325d18202d9d0fc443af080e4455ff439
Josh Symonds <josh@joshsymonds.com>
2026-09-19 18:10:32 -0700

    Add production Valheim service module

---
+++ tests/valheim.nix
@@ -0,0 +1,71 @@
+{pkgs}: let
+  inherit (pkgs) lib;
+  module = ../modules/services/valheim.nix;
+  fakePackage = pkgs.writeShellScriptBin "valheim-server" "exit 0";
+  evaluate = extraModules:
+    import "${pkgs.path}/nixos/lib/eval-config.nix" {
+      system = "x86_64-linux";
+      specialArgs = {inherit pkgs;};
+      modules = [module] ++ extraModules;
+    };
+  disabled = (evaluate []).config;
+  enabled =
+    (evaluate [
+      {
+        services.valheim = {
+          enable = true;
+          package = fakePackage;
+          passwordFile = "/run/valheim-test-password";
+        };
+      }
+    ]).config;
+  service = enabled.systemd.services.valheim;
+  unit = service.serviceConfig;
+  launcher = unit.ExecStart;
+  stopper = unit.ExecStop;
+in
+  assert !(disabled.systemd.services ? valheim);
+  assert !(disabled.users.users ? valheim);
+  assert !(disabled.users.groups ? valheim);
+  assert enabled.users.users.valheim.isSystemUser;
+  assert enabled.users.users.valheim.group == "valheim";
+  assert enabled.users.users.valheim.home == "/var/lib/valheim";
+  assert enabled.users.groups ? valheim;
+  assert service.wantedBy == ["multi-user.target"];
+  assert service.environment.HOME == "/var/lib/valheim";
+  assert unit.User == "valheim";
+  assert unit.Group == "valheim";
+  assert unit.StateDirectory == "valheim";
+  assert unit.StateDirectoryMode == "0700";
+  assert unit.WorkingDirectory == "/var/lib/valheim";
+  assert unit.Restart == "on-failure";
+  assert unit.KillSignal == "SIGINT";
+  assert unit.LimitCORE == 0;
+  assert unit.StandardOutput == "null";
+  assert unit.StandardError == "null";
+  assert unit.UMask == "0077";
+    pkgs.runCommand "valheim-fast-test" {} ''
+      set -eu
+      grep -F -- '-nographics' ${launcher}
+      grep -F -- '-batchmode' ${launcher}
+      grep -F -- '-disable-crash-handler' ${launcher}
+      grep -F -- '-logFile -' ${launcher}
+      grep -F -- '-name Midgard' ${launcher}
+      grep -F -- '-world Midgard' ${launcher}
+      grep -F -- '-port 2456' ${launcher}
+      grep -F -- '-public 0' ${launcher}
+      grep -F -- '-savedir /var/lib/valheim' ${launcher}
+      ! grep -F -- '-crossplay' ${launcher}
+      grep -F -- '/var/lib/valheim/logs' ${launcher}
+      grep -F -- 'valheim-current.log' ${launcher}
+      grep -F -- 'valheim invocation start' ${launcher}
+      grep -F -- 'valheim invocation exit status=' ${launcher}
+      grep -F -- '>> "$log_file" 2>&1' ${launcher}
+      ! grep -F -- '/bin/cat' ${launcher}
+      ! grep -F -- 'PIPESTATUS' ${launcher}
+      grep -F -- 'valheim_server.x86_64' ${stopper}
+      grep -F -- 'kill -INT' ${stopper}
+      grep -F -- 'valheim password file is missing or unreadable' ${launcher}
+      grep -F -- 'valheim password is invalid' ${launcher}
+      touch "$out"
+    ''
